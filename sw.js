d2a46b5ad5adf61add388a086acb39af75f93566
salil1gautam1-collab <salil1.gautam1@gmail.com>
2026-06-18 07:28:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE="dedicatusit-sdos-v11";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url);
+  if(u.hostname.includes("googleapis.com")||u.hostname.includes("google.com")){return;}
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp).catch(()=>{}));return resp;}).catch(()=>caches.match("./index.html"))));});
